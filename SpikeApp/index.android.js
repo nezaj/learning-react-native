777635724e0eedb06ba8e6f4cb2414d498a6cb0c
Joe Averbukh <joeaverbukh@gmail.com>
2016-06-05 18:09:49 -0700

    Add Spike Project w/ Flexbox example

---
+++ SpikeApp/index.android.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { AppRegistry } from 'react-native';
+
+import SpikeApp from './src/FlexboxSpike.react'
+
+AppRegistry.registerComponent('SpikeApp', () => SpikeApp);
